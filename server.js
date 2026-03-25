f991d8e8ccc13cde2a050531cc753de32994fd81
lelongtoan <83998616+lelongtoan@users.noreply.github.com>
2026-03-26 06:54:47 +0700

    1

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Server test is running");
 });
@@ -18,6 +20,13 @@ app.get("/snapshot", (req, res) => {
     gameDay: 1,
     gameHour: 12,
     message: "hello from render"
+  });
+});
+
+app.post("/echo-command", (req, res) => {
+  res.json({
+    ok: true,
+    received: req.body
   });
 });
 
